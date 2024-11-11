0d101486db1477bbc65b7974febd338d49289fc8
Scrappyz <ghostmic3000@gmail.com>
2024-11-11 18:49:32 +0800

    Added a little styling

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,27 @@ function App() {
 
   const button = {
     backgroundColor: "transparent",
-    color: "black",
-    borderColor: "black",
+    color: "white",
+    borderColor: "white",
+    borderWith: "20px",
+    borderRadius: "3px",
     fontSize: "80px",
     textAlign: "center"
   }
 
   return (
     <div className='frame'>
-      <TicTacToe area={3} players={['O', 'X']} checkWinner={getWinner} turnCounter={setTurn} boardStyle={board} buttonStyle={button} />
+      <div className="tictactoe-container">
+        <div className='game-container'>
+        <TicTacToe area={3} players={['O', 'X']} checkWinner={getWinner} turnCounter={setTurn} boardStyle={board} buttonStyle={button} />
+          <div className='turn-container'>
+            <p>Turn 1</p>
+          </div>
+        </div>
+        <div className='button-container'>
+          <button>Reset</button>
+        </div>
+      </div>
     </div>
   )
 }
